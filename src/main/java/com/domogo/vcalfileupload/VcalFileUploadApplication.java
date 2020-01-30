686470e0f474c5ffc2add012598ef5890891c2e7
Domogo <domsatrapa@gmail.com>
2020-01-30 17:13:45 +0100

    Initial commit

---
+++ src/main/java/com/domogo/vcalfileupload/VcalFileUploadApplication.java
@@ -0,0 +1,13 @@
+package com.domogo.vcalfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcalFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcalFileUploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domogo/vcalfileupload/VcalFileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domogo.vcalfileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcalFileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
